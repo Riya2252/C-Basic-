ee65ec3fb165eaafd9ac686fb1a5c523a2e5a98f
Riya2252 <rs2252804@gmail.com>
2024-09-20 12:15:28 +0530

    Add files via upload

---
+++ rs.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	getch();
+	float a=4.56789 , b= 123.45678;
+	printf("multiply  of two floating number is %f \n", a*b);
+	printf("multiply  of two floating number is %.1f \n", a*b);
+		printf("multiply  of two floating number is %.8f \n", a*b);
+		getch();
+	
+	
+	
+	
+}
