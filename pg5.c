cad9cc0d8dcf393b8ab8b1ad78f332302e9bb8ba
Riya2252 <rs2252804@gmail.com>
2024-09-27 12:15:57 +0530

    Add files via upload

---
+++ pg5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf(" enter any number to check is it even or odd...");
+    scanf("%d",&num);
+    if(num%2==0){
+        printf("the number is even ....");
+    }
+    else{
+        printf("the number is odd ");
+    }
+
+    
+        return 0;
+}
