944f7558137d2a7a3ffd99ec4c25afbec4e73740
Riya2252 <rs2252804@gmail.com>
2024-09-27 12:15:41 +0530

    Add files via upload

---
+++ pg4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+
+    int i;
+    float f;
+    char c;
+    double d;
+    printf(" the size of integer %zu \n",sizeof(i));
+      printf(" the size of Float %zu \n",sizeof(f));
+        printf(" the size of character %zu \n",sizeof(c));
+          printf(" the size of double  %zu \n",sizeof(d));
+
+        return 0;
+}
