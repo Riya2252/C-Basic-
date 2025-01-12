44c38cac07065096da9e65e2190988317058b984
Riya2252 <rs2252804@gmail.com>
2025-01-12 15:07:16 +0530

    Add files via upload

---
+++ butterfly.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+ void  top( int n);
+  void bottom(int n);
+    
+int main(){
+    int n ;
+    scanf("%d",&n);
+    top(n);
+  bottom(n);
+   return 0;
+    
+}
+void top(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            printf("*");
+
+        }
+        for(int j=0;j<2*(n-i-1);j++){
+            printf(" ");
+
+        }
+        for(int j=0;j<i+1;j++){
+            printf("*");
+
+        }
+    
+         printf("\n");
+         
+    }   
+   
+    
+}
+void bottom(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            printf("*");
+
+        }
+        for(int j=0;j<2*i;j++){
+            printf(" ");
+
+        }
+        for(int j=0;j<n-i;j++){
+            printf("*");
+
+        }
+    
+         printf("\n");
+         
+    }   
+    
+}
