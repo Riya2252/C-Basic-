c4ed8361851c901b1cf650b62a3c63b151f74ada
Riya2252 <rs2252804@gmail.com>
2025-01-30 07:10:25 +0530

    Add files via upload

---
+++ Recursionpg10.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int reverse(char st[],start,end){
+    
+}
+int main(){
+    return 0;
+char st[100];
+printf("Enter The string");
+scanf("%s",st);
+ 
+ int l=strlen(st);
+
+
+
+}
