2670a7de930a3465dac50d5051751ef3bb93717b
Riya2252 <rs2252804@gmail.com>
2025-01-30 20:30:34 +0530

    Add files via upload

---
+++ day2pg2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+int num1,num2,num3;
+printf("Enter three  number  ");
+scanf("%d %d %d",&num1,&num2,&num3);
+if(num1>num2 && num1>num3){
+printf("The %d   is largest Number",num1);
+}
+else if(num2>num3){
+    printf("The %d   is largest Number",num2);
+}
+else{
+   printf("The %d   is largest Number",num3);
+}
+
+    return 0;
+}
