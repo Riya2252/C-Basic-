4ead87118d6d2c952e1fc3b67c8fd60f26f69298
Riya2252 <rs2252804@gmail.com>
2025-01-30 20:34:53 +0530

    Add files via upload

---
+++ day2pg10.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n,count=0;
+    printf(" Enter the number to check if it is a prime or not  ");
+    scanf("%d",&n);
+    for(int i=2;i<=n;i++){
+        for(int j=1;j<i;i++){
+            if(i%j==0){
+                count++;
+            }
+        }
+    }
+}
